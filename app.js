75c2dd7b72ca419d2d75534ba858a8730aa7eb7e
RahulSG39 <rahulgirish39@gmail.com>
2022-03-03 10:18:29 +0530

    Used morgan middleware to log requests

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 //npm i nodemon
 //npm i express
 //npm i dotenv
+//npm i morgan
 
 const express = require("express");
+const morgan = require("morgan");
 const app = express(); //creates a web server
 
 require("dotenv/config"); //imports package to get access to .env file
 
 const api = process.env.API_URL;
 
-//Middleware - Used so that the backend understands that frontend is sending json
-app.use(express.json());
+app.use(express.json()); //Middleware - Used so that the backend understands that frontend is sending json
+app.use(morgan("tiny")); //Middleware - Used to log the http requests | tiny - argument to make brief description
 
 //Fetches content at route '/' and receives a callback
 app.get(`${api}/products`, (req, res) => {
