b8af8d5046fb07d2a097f13a75a6da92d9afaf0a
RahulSG39 <rahulgirish39@gmail.com>
2022-03-03 09:56:04 +0530

    Get post and json parsing middleware

--- app.js
+++ app.js
@@ -9,13 +9,26 @@ require("dotenv/config"); //imports package to get access to .env file
 
 const api = process.env.API_URL;
 
+//Middleware - Used so that the backend understands that frontend is sending json
+app.use(express.json());
+
 //Fetches content at route '/' and receives a callback
-app.get(api + "/products", (req, res) => {
-  res.send("Hello API");
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "hair dresser",
+    image: "some_url",
+  };
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
 });
 
 //Listens to port 3000 and on successful creation of app server it gives a callback
 app.listen(3000, () => {
   console.log("server is running at http://localhost:3000");
-  console.log(api);
 });
